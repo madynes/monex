ec5925f91f4f91ce4647d30542b978b72b80417a
Alexandre MERLIN <alexandre.merlin@inria.fr>
2018-01-10 11:02:22 +0100

    fix Artifacts many-node starting script

--- Artifacts_and_datasets/many-nodes_bittorrent/starting.rb
+++ Artifacts_and_datasets/many-nodes_bittorrent/starting.rb
@@ -3,7 +3,7 @@ require 'distem'
 
 
 Distem.client do |cl|
-  peers = (0..3).map {|x| "peer#{x.to_s.rjust(2,'0')}"}
+  peers = (0..99).map {|x| "peer#{x.to_s.rjust(2,'0')}"}
 
   peers.each do |peer|
     cl.vnode_execute(peer, "transmission-remote 127.0.0.1 -t 1 -S")
